025f327a726e0251d6e2d5183ba8979e3923b377
vasuglory <nallagorlavasantha@gmail.com>
2025-10-21 17:24:10 +0530

    complete nav-bar and static file connection in app.js.

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+const port = 4000;
+
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.get("/",(req,res)=>{
+  res.render("home");
+})
+app.listen(port,()=>{
+  console.log(`server listening on url https://localhost:${port}`);
+})
